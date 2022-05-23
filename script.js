2382731f9a9deded246802cf1d5bc2d921647ff4
Mabel Taiwo <seyitaiwo21@gmail.com>
2022-05-23 11:04:47 +0100

    trenches

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ menu.addEventListener('click', () => {
   menu.classList.toggle('change')
 })
 // End of Navbar
+
+// section 2 video
+const video = document.querySelector('.video')
+const btn = document.querySelector('.buttons i') 
+const bar = document.querySelector('.video bar')
+
+const playPause = () => {
+  if(video.paused) {
+    video.play()
+    btn.className = 'far fa-pause-circle'
+    video.style.pacity = '0.7'
+  } else {
+     video.pause()
+    btn.className = 'far fa-play-circle'
+    video.style.pacity = "0.3"
+  }
+}
+
+btn.addEventListener('click', () => {
+  playPause()
+})
+// End of section 2 video
